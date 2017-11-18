838866ce91a9559a54ce147ca6e5490e9bb863fc
Radi Cholakov <radicho123@gmail.com>
2017-11-18 09:46:11 +0200

    Filesystem changed.

---
+++ development/js/sizes.js
@@ -0,0 +1,19 @@
+const getSizes = () => {
+	const width = window.innerWidth,
+				height = window.innerHeight;
+				
+	const sizes = {};
+
+	if (width > height) {
+		sizes.height = height / 1.05;
+		sizes.width = sizes.height;
+	} else {
+		sizes.width = width / 1.05;
+		sizes.height = sizes.width;
+	}
+	sizes.fontSize = sizes.width / 20;
+
+	return sizes;
+}
+
+export default getSizes;
