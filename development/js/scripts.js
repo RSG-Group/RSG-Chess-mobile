417888f03757dd5871fe6537df1376fa448494cc
Radi Cholakov <radicho123@gmail.com>
2018-04-05 18:05:20 +0300

    Add color palettes.

--- development/js/scripts.js
+++ development/js/scripts.js
@@ -25,6 +25,42 @@ let palettes = [
       selectedBG: 'brown',
       selectedColor: 'lightblue'
     }
+  },
+  {
+    id: 2,
+    name: 'Light blue ocean',
+    background: 'rgb(220, 255, 245)',
+    props: {
+      whiteCells: 'rgb(225, 225, 225)',
+      blackCells: 'rgb(110, 130, 180)',
+      validBG: 'rgb(255, 100, 45)',
+      selectedBG: 'rgb(155, 0, 0)',
+      selectedColor: 'lightgreen'
+    }
+  },
+  {
+    id: 3,
+    name: 'Chess.com green',
+    background: 'rgb(235, 255, 205)',
+    props: {
+      whiteCells: 'rgb(240, 240, 210)',
+      blackCells: 'rgb(120, 150, 85)',
+      validBG: 'rgb(255, 253, 158)',
+      selectedBG: 'rgb(70, 0, 165)',
+      selectedColor: 'rgb(255, 255, 155)'
+    }
+  },
+  {
+    id: 4,
+    name: 'Sweety',
+    background: 'rgb(255, 245, 225)',
+    props: {
+      whiteCells: 'rgb(255, 215, 215)',
+      blackCells: 'rgb(230, 110, 110)',
+      validBG: 'rgb(70, 0, 165)',
+      selectedBG: 'rgb(185, 75, 0)',
+      selectedColor: 'rgb(135, 255, 235)'
+    }
   }
 ]
 
