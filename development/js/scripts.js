c3aedf4b18ec3bc837396ffc1c58159ecf61419e
Radi Cholakov <radicho123@gmail.com>
2018-04-27 18:35:50 +0300

    Create config.js and move the color palettes there.

--- development/js/scripts.js
+++ development/js/scripts.js
@@ -12,61 +12,11 @@ import _ from 'lodash'
 import classNames from 'classnames'
 import { Game, Pieces } from 'rsg-chess'
 import Graphics from 'rsg-chess-graphics'
+import { colorPalettes as palettes } from './config'
 import getSizes from './sizes'
 
 // import a web worker for AI calculations.
 var worker = new Worker('src/worker.min.js')
-
-let palettes = [
-  {
-    id: 1,
-    name: 'RSG Chess standard',
-    background: 'rgb(240, 220, 180)',
-    props: {
-      whiteCells: 'rgb(255, 205, 160)',
-      blackCells: 'rgb(210, 140, 70)',
-      validBG: 'red',
-      selectedBG: 'brown',
-      selectedColor: 'lightblue'
-    }
-  },
-  {
-    id: 2,
-    name: 'Light blue ocean',
-    background: 'rgb(220, 255, 245)',
-    props: {
-      whiteCells: 'rgb(225, 225, 225)',
-      blackCells: 'rgb(110, 130, 180)',
-      validBG: 'rgb(255, 100, 45)',
-      selectedBG: 'rgb(155, 0, 0)',
-      selectedColor: 'lightgreen'
-    }
-  },
-  {
-    id: 3,
-    name: 'Chess.com green',
-    background: 'rgb(235, 255, 205)',
-    props: {
-      whiteCells: 'rgb(240, 240, 210)',
-      blackCells: 'rgb(120, 150, 85)',
-      validBG: 'rgb(255, 253, 158)',
-      selectedBG: 'rgb(70, 0, 165)',
-      selectedColor: 'rgb(255, 255, 155)'
-    }
-  },
-  {
-    id: 4,
-    name: 'Sweety',
-    background: 'rgb(255, 245, 225)',
-    props: {
-      whiteCells: 'rgb(255, 215, 215)',
-      blackCells: 'rgb(230, 110, 110)',
-      validBG: 'rgb(70, 0, 165)',
-      selectedBG: 'rgb(185, 75, 0)',
-      selectedColor: 'rgb(135, 255, 235)'
-    }
-  }
-]
 
 // initialize game
 let game
