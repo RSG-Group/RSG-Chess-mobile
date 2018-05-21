46562f6020b54c866a8c27ff6efa855e1caae07f
Radi Cholakov <radicho123@gmail.com>
2018-05-21 13:16:07 +0300

    Add basic `handlePress` method and simple piece movement.

--- App.js
+++ App.js
@@ -13,7 +13,8 @@ export default class App extends Component<Props> {
     super();
     this.state = {
       width: Dimensions.get("window").width,
-      height: Dimensions.get("window").height
+      height: Dimensions.get("window").height,
+      selected: null
     };
 
     Dimensions.addEventListener("change", () => {
@@ -40,22 +41,59 @@ export default class App extends Component<Props> {
     return sizes;
   }
 
+  handlePress(x, y) {
+    let { selected, /* playAgainstAI, isAIThinking */ } = this.state
+
+    // if (isAIThinking) {
+    //   if (window.plugins && window.plugins.toast) {
+    //     window.plugins.toast.show('Plase wait while our AI is thinking...', '1750', 'bottom')
+    //   }
+    //   return
+    // }
+
+    if (selected) {
+      // move the selected piece
+      let moved = game.moveSelected(
+        selected, {x: x, y: y}, this.__handlePromotion, this.__handleCheckmate, false
+      )
+      this.setState({ selected: null })
+
+      // use the worker for generating AI movement
+
+      // let last = game.turn.length - 1
+      // if (moved && playAgainstAI && last >= 0 && game.turn[last].color === 'W') {
+      //   worker.postMessage({ game, playAgainstAI })
+      //   this.setState({ isAIThinking: true })
+      // }
+    } else {
+      let last = game.turn.length - 1
+      if (
+        game.board[y][x] &&
+        (last >= 0 ? game.board[y][x].color !== game.turn[last].color
+          : game.board[y][x].color === 'W')
+      ) {
+        this.setState({ selected: game.board[y][x] })
+      } else {
+        game.board[y][x] && alert('Invalid Move!')
+      }
+    }
+  }
+
   render() {
     const sizes = this.getSizes();
+    const { selected } = this.state;
 
     return (
       <View onLayout={this._onLayout} style={styles.container}>
         <ChessBoard
+          self={this}
           board={game.board}
           boardWidth={sizes.width}
           boardHeight={sizes.height}
-          pieceSize={sizes.fontSize}
+          selected={selected}
           showValidMoves={true}
-          selected={game.board[1][1]}
-          self={this}
-          onPress={(x, y) => {
-            alert(`x: ${x}, y: ${y};`);
-          }}
+          pieceSize={sizes.fontSize}
+          onPress={this.handlePress}
         />
       </View>
     );
