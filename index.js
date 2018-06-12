26521931595a31aa3287a911d99cb897f1cdffbd
Radi Cho <radicho123@gmail.com>
2018-06-12 18:19:17 +0300

    Add settings components and adapt the layout.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+console.disableYellowBox = true;
 AppRegistry.registerComponent('RSG_Chess_mobile_native', () => App);
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { Platform, View, Text } from "react-native";
+import { Platform, View, Text, ScrollView, Dimensions } from "react-native";
 import firebase from "react-native-firebase";
 import CheckmateSnackBar from "../components/CheckmateSnackBar";
+import {
+  SettingsDividerShort,
+  SettingsDividerLong,
+  SettingsEditText,
+  SettingsCategoryHeader,
+  SettingsSwitch,
+  SettingsPicker
+} from "react-native-settings-components";
 
 export default class HomePage extends Component<Props> {
   static navigationOptions = {
@@ -14,6 +22,12 @@ export default class HomePage extends Component<Props> {
     const AdRequest = firebase.admob.AdRequest;
     this.request = new AdRequest();
     this.request.addKeyword("foobar");
+
+    this.state = {
+      username: "",
+      allowPushNotifications: false,
+      gender: ""
+    };
   }
 
   render() {
@@ -23,14 +37,42 @@ export default class HomePage extends Component<Props> {
       <View
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
+          justifyContent: "flex-start",
+          alignItems: "flex-start"
         }}
       >
-        <View>
-          <Text>Settings</Text>
-        </View>
-        <View style={{ height: 100 }}>
+        <ScrollView
+          contentContainerStyle={{
+            flex: 1,
+            width: Dimensions.get("window").width
+          }}
+        >
+          <SettingsCategoryHeader title={"Personalize"} />
+          {/* <Text>
+            Note that these settings are session only. If you restart the app
+            they won't be saved.
+          </Text> */}
+          <SettingsDividerLong android={true} />
+          <SettingsPicker
+            title="Color palettes"
+            dialogDescription="Choose one of our color palettes."
+            possibleValues={[
+              { label: "...", value: "" },
+              { label: "male", value: "male" },
+              { label: "female", value: "female" },
+              { label: "other", value: "other" }
+            ]}
+            negativeButtonTitle={"Cancel"}
+            positiveButtonTitle={"Okay"}
+            onSaveValue={value => {
+              this.setState({
+                gender: value
+              });
+            }}
+            value={this.state.gender}
+          />
+        </ScrollView>
+        <View style={{ height: 52 }}>
           <Banner
             size={"SMART_BANNER"}
             request={request.build()}
