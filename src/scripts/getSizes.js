9ba35d9be78f45c2ff5b4d76ab472395d47decc5
Radi Cho <radicho123@gmail.com>
2018-06-20 11:44:48 +0300

    Update the getSizes function.

--- src/scripts/getSizes.js
+++ src/scripts/getSizes.js
@@ -2,13 +2,15 @@ const getSizes = (width, height) => {
   const sizes = {};
 
   if (width > height) {
-    sizes.height = Math.floor(height / 8 / 4) * 4;
+    sizes.height = height / 8;
     sizes.width = sizes.height;
   } else {
-    sizes.width = Math.floor(width / 8 / 4) * 4;
+    sizes.width = width / 8;
     sizes.height = sizes.width;
   }
   sizes.fontSize = Math.floor(sizes.width / 1.185);
+  sizes.width = Math.floor(sizes.width / 4) * 4;
+  sizes.height = Math.floor(sizes.height / 4) * 4;
 
   return sizes;
 };
