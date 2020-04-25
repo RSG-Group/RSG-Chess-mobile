8aa166ea127ed354582b9922bbc355964b63afe1
Radi Cho <radicho123@gmail.com>
2020-04-25 22:59:08 +0300

    Puzzles page basic layout.

--- src/pages/Puzzles.js
+++ src/pages/Puzzles.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text, StatusBar, Button } from "react-native";
 import firebase from "react-native-firebase";
 import NavigationContext from "../components/NavigationContext";
+import { puzzles } from "../config";
+import { Game } from "rsg-chess";
+import ChessBoard from "rsg-chess-rn-graphics";
+import getSizes from "../scripts/getSizes";
+
+/// /// ///
+/// TODO
+/// UPDATE THE PAGE TO BE RESPONSIVE FOR DIFFERENT DISPLAY SIZES
+/// UPDATE THE PAGE TO BE RESPONSIVE FOR LANDSCAPE
+/// UPDATE THE PAGE FOR INTERNATIONALIZATION
+/// ADD PUZZLE BACK AND FORTH FUNCTIONALITY
+/// INTEGRATE WITH SAVEDPREFERENCES AND INDICATE ALREADY PLAYED PUZZLES
+/// TODO
+/// /// ///
 
 export default class Puzzles extends Component<Props> {
   static navigationOptions = {
     title: "Puzzles",
     header: null,
-    drawerLabel: () => <View></View>
+    drawerLabel: () => <View></View>,
+    headerLeft: null
   };
 
   componentDidMount() {
@@ -16,12 +31,55 @@ export default class Puzzles extends Component<Props> {
 
   render() {
     return (
-      <View>
-        <Text>123</Text>
-        <NavigationContext.Consumer>
-          {data => {}}
-        </NavigationContext.Consumer>
-      </View>
+      <NavigationContext.Consumer>
+        {data => {
+          const { width, height } = data;
+          let sizes = getSizes(width, height);
+          let puzzle = puzzles[0];
+          let localGame = Game.prototype.initializeGame();
+          localGame.initGameFEN(puzzle.fen);
+
+          return (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <StatusBar hidden={true} />
+              <View>
+                <View style={{
+                  top: 75,
+                  width: sizes.width * 6,
+                  position: "absolute",
+                  alignSelf: "flex-start"
+                }}>
+                  <Text style={{ textAlign: "center", fontSize: 20, color: "black" }}>{puzzle.players}</Text>
+                  <Text style={{ textAlign: "center", fontSize: 18 }}>Year played: {puzzle.date}</Text>
+                </View>
+                <ChessBoard
+                  board={localGame.board}
+                  boardWidth={sizes.width * 0.75}
+                  boardHeight={sizes.height * 0.75}
+                  pieceSize={sizes.fontSize * 0.75}
+                />
+                <View style={{
+                  flexDirection: 'row',
+                  width: sizes.width * 6,
+                  bottom: 130,
+                  position: "absolute",
+                  alignSelf: "flex-start"
+                }}>
+                  <View style={{ flex: 1, margin: 3 }}><Button disabled title={"<"} /></View>
+                  <View style={{ flex: 5, margin: 3 }}><Button title={"Play"} /></View>
+                  <View style={{ flex: 1, margin: 3 }}><Button title={">"} /></View>
+                </View>
+              </View>
+            </View>
+          )
+        }}
+      </NavigationContext.Consumer>
     );
   }
 }
