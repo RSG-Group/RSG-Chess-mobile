3667ea21992a71ea067213f6ade084c7fa35f3f4
Radi Cho <radicho123@gmail.com>
2020-05-06 12:18:38 +0300

    Incremental update (alpha.2)

--- src/scripts/puzzleHelper.js
+++ src/scripts/puzzleHelper.js
@@ -1,21 +1,5 @@
 import { Pieces } from "rsg-chess";
 
-JSON.safeStringify = (obj, indent = 2) => {
-  let cache = [];
-  const retVal = JSON.stringify(
-    obj,
-    (key, value) =>
-      typeof value === "object" && value !== null
-        ? cache.includes(value)
-          ? undefined // Duplicate reference found, discard key
-          : cache.push(value) && value // Store value in our collection
-        : value,
-    indent
-  );
-  cache = null;
-  return retVal;
-};
-
 const validKingMoves = function (simulate) {
   var moves = [];
   var coordinates = [
